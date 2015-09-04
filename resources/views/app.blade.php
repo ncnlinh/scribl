5e5abdb749af7f0dfb1936875f9d459ff37ff1e9
yzxj <yeozx@comp.nus.edu.sg>
2015-09-04 09:17:59 +0800

    Minor: Added GIF Icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -114,7 +114,7 @@
       <canvas resize="true" id="canvas" width="800px" height="600px">
         This text is displayed if your browser does not support HTML5 Canvas.
       </canvas>
-      <button type="button" id="postOnFacebookPromptBtn" class="btn btn-primary">Send</button>
+      <button type="button" id="postOnFacebookPromptBtn" class="btn btn-primary">Share this drawing!</button>
     </td>
   </tr>
   </table>
